62be438279b43c10485214d112e0adbb9d7685bc
Yuvrajsanap <125144651+Yuvrajsanap@users.noreply.github.com>
2024-05-24 00:16:49 +0530

    Delete Assignment-2/packages directory

--- Assignment-2/packages/question3.java
+++
@@ -1,8 +0,0 @@
-import java.net.URL;
-
-public class question3 {
-    public static void main(String[] args) throws Exception {
-        URL url = new URL("https://www.example.com");
-        System.out.println("Website exists: " + url.openConnection().getContent() != null);
-    }
-}
--- Assignment-2/packages/question5.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class question5 {           //ArraySort in ascending order
-    public static void main(String[] args) {
-        int[] numbers = {3, 1, 4, 2};
-        Arrays.sort(numbers);  
-        System.out.println("Sorted numbers: " + Arrays.toString(numbers));  
-    }
-}
